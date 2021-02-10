0b3f20aecac7938964d991cada3ab3e1dc392370
Zhupar Adamova <zhuparadamova@gmail.com>
2021-02-11 03:42:51 +0600

    created a file figure

---
+++ figure.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"x: {self.x}, y: {self.y}, width: {self.width}, height: {self.height}"
+
+
+r1 = Rectangle(5, 10, 50, 100)
+print(r1)
